f1b6dc91d95e850ed8647c936d11c928275b9992
Andrew Sherman <andrew@ohair-sherman.com>
2016-07-28 11:39:20 -0700

    Experiment to show expected value of rolling dice until getting a 1

--- dice/src/com/bartash/dice/Main.java
+++ dice/src/com/bartash/dice/Main.java
@@ -5,6 +5,23 @@ public class Main
 
     public static void main(String[] args)
     {
-        // write your code here
+        int[] dice = {12, 10, 8, 6, 4};
+
+        for (int size : dice)
+        {
+            double total = 0;
+            double chance = (double) (size - 1) / (double) size;
+            for (int j = 0; j < 100; j++)
+            {
+                double delta = Math.pow(chance, j);
+                total += delta;
+                if (delta < 0.01)
+                {
+                    break;
+                }
+            }
+            // confirm result at https://math.stackexchange.com/questions/698177/expected-value-of-rolling-dice-until-getting-a-3
+            System.out.println("total for d" + size + " = " + total);
+        }
     }
 }
