adb7e29dc7a38cf6b23a2a4106da465b8a379ff3
Andrew Sherman <asherman@cloudera.com>
2018-06-14 10:54:50 -0700

    properly spot when first replace did not change things

--- dice/src/com/bartash/dice/FixNumFilesLine.java
+++ dice/src/com/bartash/dice/FixNumFilesLine.java
@@ -55,7 +55,7 @@ public class FixNumFilesLine {
     // sometimes the output has all the numbers lined up
     String newLine = line.replaceFirst("numFiles {11}", "numFilesErasureCoded")
         .replaceFirst("\\d+", "0");
-    if (newLine.contains("numFiles")) {
+    if (!newLine.contains("numFilesErasureCoded")) {
       // if the numbers weren't all lined up
       newLine = line.replaceFirst("numFiles", "numFilesErasureCoded");
     }
