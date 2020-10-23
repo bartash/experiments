7f65ebc97e4af151ba07e5a954dfb96f6a64932d
Andrew Sherman <asherman@cloudera.com>
2020-10-23 13:32:13 -0700

    try using url from vw

--- impala-jdbc/src/com/bartash/jdbc/impala/impala.java
+++ impala-jdbc/src/com/bartash/jdbc/impala/impala.java
@@ -29,7 +29,7 @@ public class impala {
     // jdbc:impala://coordinator-asherman-run-run-run.env-sxmzbh.dwx-dev-public.cloudera.com:443/default;AuthMech=3;transportMode=http;httpPath=cliservice;ssl=1
     try (Connection con = DriverManager
         .getConnection(
-            "jdbc:impala://localhost:28000/default;transportMode=http;httpPath=cliservice;AuthMech=0")) {
+            "jdbc:impala://impala-proxy-asherman-proxy.env-zrd68v.local.dwx.dev.cldr.work:443/default;AuthMech=3;transportMode=http;httpPath=cliservice;ssl=1")) {
       System.out.println("connected");
       // create statement
       Statement stmt = con.createStatement();
